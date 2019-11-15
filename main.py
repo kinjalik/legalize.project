5f25ec77da91f89c56092be925f242b903391205
aramann <artem.amosov@mail.ru>
2019-11-16 00:56:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pyshark
-
-cap = pyshark.FileCapture('cap.pcap')
 # в ваершарке прописываем ключи
 # 01:03:05:07:09:0B:0D:0F:00:02:04:06:08:0A:0C:0D
 # 5A:69:67:42:65:65:41:6C:6C:69:61:6E:63:65:30:39
 
+import pyshark
+cap = pyshark.LiveCapture(interface='en0')
 num = 1
-for pack in cap:
+for pack in cap.sniff_continuously():
+    print(1)
     try:
         aps = str(pack['ZBEE_APS']).split()
         ind = aps.index('Cluster:')
@@ -35,9 +35,11 @@ for pack in cap:
 
     if sensor == ' Temperature Measurement (0x0402)' or sensor == ' Relative Humidity Measurement (0x0405)':
         ret['value'] = value[:-2] + ',' + value[-2:]
-        print(value[:-2] + ',' + value[-2:])
+        # print(value[:-2] + ',' + value[-2:])
     else:
         ret['value'] = pack['ZBEE_ZCL'].get_field_by_showname('Measured Value')
         # print(pack['ZBEE_ZCL'].get_field_by_showname('Measured Value'))
-    print(ret)
     num += 1
+
+    # {'sensor': sensor, 'value': value}
+    print(ret)
